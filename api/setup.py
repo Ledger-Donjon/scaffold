c74d6609618c21dc0301cb6c794fe8b68e09a26b
Olivier Hériveaux <olivier.heriveaux@ledger.fr>
2020-10-06 14:29:28 +0200

    Updated version number for pypi

--- api/setup.py
+++ api/setup.py
@@ -26,7 +26,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="donjon-scaffold",
-    version="0.7",
+    version="0.7.1",
     author="Olivier Heriveaux",
     description="Python3 API for the Scaffold board",
     long_description=long_description,
