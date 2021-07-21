c2080a171ce6b575d9e3b2eca2fb8e1361554bd7
Olivier Hériveaux <olivier.heriveaux@ledger.fr>
2021-07-21 14:02:15 +0200

    Minor fix in sphinx doc configuration

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ sys.path.insert(0, os.path.abspath('../api'))
 # ones.
 extensions = ['sphinx.ext.mathjax', 'sphinxcontrib.wavedrom',
     'sphinxcontrib.spelling', 'sphinx.ext.graphviz', 'modbox',
-    'sphinx.ext.autodoc']
+    'sphinx.ext.autodoc', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
