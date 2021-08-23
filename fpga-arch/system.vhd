056acde8aab1c7404dd06f824c619e1aa60463c7
Olivier Hériveaux <olivier.heriveaux@ledger.fr>
2021-08-23 16:41:11 +0200

    Fixed bus bridge bug on read operations

--- fpga-arch/system.vhd
+++ fpga-arch/system.vhd
@@ -488,7 +488,7 @@ begin
 
     -- Version module
     e_version_module: entity work.version_module
-    generic map (version => "scaffold-0.7")
+    generic map (version => "scaffold-0.7.1")
     port map (
         clock => clock,
         reset_n => reset_n,
