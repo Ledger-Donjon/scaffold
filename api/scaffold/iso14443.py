bc43a14980cd8801311b740ffbff57fe7104089a
Olivier Hériveaux <olivier.heriveaux@ledger.fr>
2025-06-17 13:39:44 +0200

    ISO14443 documentation and lint

--- api/scaffold/iso14443.py
+++ api/scaffold/iso14443.py
@@ -21,6 +21,8 @@ from . import Scaffold, Pull
 from time import sleep
 from enum import Enum
 import crcmod
+import colorama
+import time
 
 
 class CRCError(Exception):
@@ -36,12 +38,13 @@ class CRCError(Exception):
 
 class TRF7970ACommand(int, Enum):
     """TRF7970A command code, as defined in the datasheet."""
+
     IDLE = 0x00
     SOFTWARE_INIT = 0x03
     RF_COLLISION_AVOIDANCE = 0x04
     RESPONSE_RF_COLLISION_AVOIDANCE_1 = 0x05
     RESPONSE_RF_COLLISION_AVOIDANCE_2 = 0x06
-    RESET_FIFO = 0x0f
+    RESET_FIFO = 0x0F
     TRANSMISSION_WITHOUT_CRC = 0x10
     TRANSMISSION_WITH_CRC = 0x11
     DELAYED_TRANSMISSION_WITHOUT_CRC = 0x12
@@ -55,6 +58,7 @@ class TRF7970ACommand(int, Enum):
 
 class TRF7970ARegister(int, Enum):
     """TRF7970A register addresses, as defined in the datasheet."""
+
     CHIP_STATUS_CONTROL = 0x00
     ISO_CONTROL = 0x01
     ISO_IEC_14443B_TX_OPTIONS = 0x02
@@ -65,8 +69,8 @@ class TRF7970ARegister(int, Enum):
     RX_NO_RESPONSE_WAIT_TIME = 0x07
     RX_WAIT_TIME = 0x08
     MODULATOR_AND_SYS_CLK_CONTROL = 0x09
-    RX_SPECIAL_SETTING = 0x0a
-    REGULATOR_AND_IO_CONTROL = 0x0b
+    RX_SPECIAL_SETTING = 0x0A
+    REGULATOR_AND_IO_CONTROL = 0x0B
     SPECIAL_FUNCTION_1 = 0x10
     SPECIAL_FUNCTION_2 = 0x11
     ADJUSTABLE_FIFO_IRQ_LEVELS = 0x14
@@ -74,18 +78,18 @@ class TRF7970ARegister(int, Enum):
     NFCID1_NUMBER = 0x17
     NFC_TARGET_DETECTION_LEVEL = 0x18
     NFC_TARGET_PROTOCOL = 0x19
-    IRQ_STATUS = 0x0c
-    COLLISION_POSITION_AND_INTERRUPT_MASK = 0x0d
-    COLLISION_POSITION = 0x0e
-    RSSI_LEVELS_AND_OSCILLATOR_STATUS = 0x0f
+    IRQ_STATUS = 0x0C
+    COLLISION_POSITION_AND_INTERRUPT_MASK = 0x0D
+    COLLISION_POSITION = 0x0E
+    RSSI_LEVELS_AND_OSCILLATOR_STATUS = 0x0F
     RAM_1 = 0x12
     RAM_2 = 0x13
-    TEST_1 = 0x1a
-    TEST_2 = 0x1b
-    FIFO_STATUS = 0x1c
-    TX_LENGTH_BYTE_1 = 0x1d
-    TX_LENGTH_BUTE_2 = 0x1e
-    FIFO_IO = 0x1f
+    TEST_1 = 0x1A
+    TEST_2 = 0x1B
+    FIFO_STATUS = 0x1C
+    TX_LENGTH_BYTE_1 = 0x1D
+    TX_LENGTH_BUTE_2 = 0x1E
+    FIFO_IO = 0x1F
 
 
 class NFC:
@@ -113,6 +117,7 @@ class NFC:
     - D14: EN
     - D15: SYS_CLK
     """
+
     def __init__(self, scaffold: Scaffold):
         self.scaffold = scaffold
         scaffold.power.dut = 0
@@ -147,6 +152,9 @@ class NFC:
         scaffold.d1.pull = Pull.UP
         self.crc_a = crcmod.mkCrcFun(0x11021, 0x6363, rev=True)
 
+        self.verbose = False
+        self.log_t_start = None
+
     def startup(self):
         """Power-on the NFC daughterboard and initialize the TRF7970A front-end."""
         self.scaffold.power.dut = 1
@@ -167,7 +175,7 @@ class NFC:
             (TRF7970ARegister.ISO_CONTROL, 0x21),
             (TRF7970ARegister.ISO_IEC_14443B_TX_OPTIONS, 0x00),
             (TRF7970ARegister.ISO_IEC_14443A_HIGH_BIT_RATE_OPTIONS, 0x00),
-            (TRF7970ARegister.TX_TIMER_CONTROL_HIGH, 0xc1),
+            (TRF7970ARegister.TX_TIMER_CONTROL_HIGH, 0xC1),
             # We could read more, but this seems good enough to be confident.
         )
         for reg, value in regs:
@@ -182,8 +190,7 @@ class NFC:
         # TODO bit 0 to 0
         self.register_write(TRF7970ARegister.CHIP_STATUS_CONTROL, 0b00100001)
         self.register_write(
-            TRF7970ARegister.CHIP_STATUS_CONTROL, 0b01100001,
-            keep_ss_low=True
+            TRF7970ARegister.CHIP_STATUS_CONTROL, 0b01100001, keep_ss_low=True
         )
         self.spi.transmit(0)
         self.iso14443.power_on()
@@ -229,22 +236,109 @@ class NFC:
 
     def reqa(self, read=True) -> bytes:
         """Sends REQA frame. Returns ATQA response bytes."""
-        self.iso14443.transmit_short(0x26)
+        data = 0x26
+        if self.verbose:
+            self.log(True, "REQA", bytes((data,)))
+        self.iso14443.transmit_short(data)
         result = self.iso14443.receive()
+        if self.verbose:
+            self.log(True, "ATQA", result)
         assert len(result) == 2
         return result
 
-    def transmit(self, data: bytes):
-        self.iso14443.transmit(data)
-        return self.iso14443.receive()
+    def log(self, rw: bool, what: str, data: bytes):
+        """
+        Print log line in standard output.
 
-    def transmit_with_crc(self, data: bytes):
-        frame = data + self.crc_a(data).to_bytes(2, "little")
-        self.iso14443.transmit(frame)
+        :param rw: True if data is sent to the card, card if data is read from the card.
+        :param str: Frame type indication ("REQA", "ATQA", etc.).
+        :param data: Data read/written from/to the card.
+        """
+        if rw:
+            print(colorama.Fore.YELLOW, end="")
+        else:
+            print(colorama.Fore.CYAN, end="")
+        sep = "│"
+        rw_str = {False: "←", True: "→"}[rw]
+        desc_lines = []
+        while len(data) > 0:
+            desc_lines.append(data[:32].hex())
+            data = data[32:]
+        if len(desc_lines) == 0:
+            desc_lines.append("")
+        t = time.time()
+        if self.log_t_start is None:
+            self.log_t_start = t
+        dt = t - self.log_t_start
+        print(
+            f"{dt * 1000.0:>9.3f} ms {sep} Scaffold {rw_str} Card {sep} {what:<4} {sep} {desc_lines[0]}"
+        )
+        for line in desc_lines[1:]:
+            print(" " * 13 + sep + " " * 17 + sep + " " * 6 + sep + f" {line}")
+        print(colorama.Fore.RESET, end="")
+
+    def transmit(self, data: bytes, what_tx: str = "", what_rx: str = "") -> bytes:
+        """
+        Transmits data to the card and reads the response. If card does not respond an
+        empty byte string is returned.
+
+        :param data: Bytes to be transmitted.
+        :param what_tx: Transmitted frame type indication ("REQA" for instance).
+            used when verbose is True.
+        :param what_rx: Received frame type indication ("ATQA" for instance).
+            used when verbose is True.
+        """
+        if self.verbose:
+            self.log(True, what_tx, data)
+        self.iso14443.transmit(data)
         response = self.iso14443.receive()
+        if self.verbose:
+            self.log(False, what_rx, response)
+        return response
+
+    def transmit_with_crc(
+        self, data: bytes, what_tx: str = "", what_rx: str = ""
+    ) -> bytes:
+        """
+        Transmits data, with CRC appended, reads the response and checks the received CRC
+        validity.
+
+        :param data: Bytes to be transmitted.
+        :param what_tx: Transmitted frame type indication ("SEL" for instance).
+            used when verbose is True.
+        :param what_rx: Received frame type indication ("SAK" for instance).
+            used when verbose is True.
+        :raises CRCError: If received CRC is invalid.
+        """
+        frame = data + self.crc_a(data).to_bytes(2, "little")
+        response = self.transmit(frame, what_tx=what_tx, what_rx=what_rx)
         assert len(response) >= 2
         got_crc = response[-2:]
         expected_crc = self.crc_a(response[:-2]).to_bytes(2, "little")
         if got_crc != expected_crc:
             raise CRCError(got_crc, expected_crc)
         return response[:-2]
+
+    def sel(self, level: int) -> bytes:
+        """
+        Performs selection. This sends a SEL frame, reads the returned UID by the card
+        and selects it with another SEL frame. Returns the UID returned during selection.
+        Note: the returned UID can be partial, only the bytes corresponding to the
+        selected cascade level are returned.
+
+        :param level: Cascade level. Must be 1, 2 or 3.
+        """
+        sel = 0x93 + 2 * (level - 1)
+        uid = self.transmit(bytes((sel, 0x20)), what_tx="SEL", what_rx="UID")
+        self.transmit_with_crc(bytes((sel, 0x70)) + uid, what_tx="SEL", what_rx="SAK")
+        return uid
+
+    def rats(self) -> bytes:
+        """
+        Sends RATS (Request for Answer To Select) frame and returns PICC
+        response.
+
+        FSDI is set to 256 bytes. This value is the highest possible and is
+        supported by Scaffold. CID is set to 0.
+        """
+        return self.transmit_with_crc(b"\xe0\x80", what_tx="RATS", what_rx="ATS")
---
+++ examples/iso14443.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+#
+# This file is part of Scaffold
+#
+# Scaffold is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+# Copyright 2025 Ledger SAS, written by Olivier Hériveaux
+
+
+from scaffold import Scaffold, ISO14443Trigger
+from scaffold.iso14443 import NFC
+
+s = Scaffold()
+nfc = NFC(s)
+s.d5 << nfc.trigger
+nfc.startup()
+nfc.iso14443.trigger_mode = ISO14443Trigger.START
+nfc.iso14443.timeout = 1
+nfc.verbose = True
+
+# REQA
+atqa = nfc.reqa()
+uid_size = ((atqa[0] >> 6) & 3) + 1
+# Selection
+for i in range(uid_size):
+    nfc.sel(i + 1)
+# Send Request Answer To Select
+result = nfc.rats()
+# Send APDU
+response = nfc.transmit_with_crc(
+    bytes.fromhex("0200a404000e315041592e5359532e4444463031")
+)
